0d12b2985be0ea755f2e1d08527d201ca9cd349d
张晓飞 <315082431@qq.com>
2017-03-06 20:38:04 +0800

    评估模块

--- CarDetecion/ViewController.swift
+++ CarDetecion/ViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum ModalPresentingType {
+    case Present, Dismiss
+}
+
+class ViewController: UIViewController , UIViewControllerTransitioningDelegate , UIViewControllerAnimatedTransitioning{
+    
+    var modalPresentingType: ModalPresentingType?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func test(_ sender: Any) {
+        let cameraViewController = CameraViewController(croppingEnabled: false, allowsLibraryAccess: true) { [weak self] image, asset in
+            
+            self?.dismiss(animated: true, completion: nil)
+        }
+        present(cameraViewController, animated: true, completion: nil)
+    }
+    
+    // 转场动画
+    
+    
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        
+        let toViewController = transitionContext.viewController(forKey: .to)
+        let fromViewController = transitionContext.viewController(forKey: .from)
+        
+        var destView: UIView!
+        var destTransfrom = CGAffineTransform.identity
+        
+        if modalPresentingType == ModalPresentingType.Present {
+            destView = toViewController!.view
+            destView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_2))
+            containerView.addSubview(toViewController!.view)
+        } else if modalPresentingType == ModalPresentingType.Dismiss {
+            destView = fromViewController!.view
+            destTransfrom = CGAffineTransform(rotationAngle: -CGFloat(M_PI_2))
+            containerView.insertSubview(toViewController!.view, belowSubview: fromViewController!.view)
+        }
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0,
+                                   options: UIViewAnimationOptions.curveLinear, animations: {
+                                    destView.transform = destTransfrom
+        }, completion: {completed in
+            transitionContext.completeTransition(true)
+        })
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let modal = segue.destination
+        modal.transitioningDelegate = self
+    }
+    
+    //UIViewControllerTransitioningDelegate
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        modalPresentingType = .Present
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        modalPresentingType = .Dismiss
+        return self
+    }
 
 }
 
